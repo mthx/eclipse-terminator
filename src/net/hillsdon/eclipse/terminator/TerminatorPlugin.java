7293410e3178121212c42d8f074ede6b0c80f335
Matt Hillsdon <matt@hillsdon.net>
2009-03-21 15:43:59 +0000

    Various terminal initialization improvements:  * Log to the correct location (though we don't ensure sane permissions    on that directory, that's 1.6 only API).  * Read preferences from the correct location & initalize the preferences.

--- src/net/hillsdon/eclipse/terminator/TerminatorPlugin.java
+++ src/net/hillsdon/eclipse/terminator/TerminatorPlugin.java
@@ -6,11 +6,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import javax.swing.UIManager;
-
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
+import terminator.Terminator;
 import e.util.FileUtilities;
 
 /**
@@ -20,6 +19,8 @@ import e.util.FileUtilities;
  */
 public class TerminatorPlugin extends AbstractUIPlugin {
 
+  private static final String HOME_DIR = System.getProperty("user.home");
+
   @Override
   public void start(BundleContext context) throws Exception {
     super.start(context);
@@ -27,20 +28,32 @@ public class TerminatorPlugin extends AbstractUIPlugin {
     // This seems to be unstable on Gtk.
     //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
     
-    // These are all rather dubious...
+    // FIXME: Bundle the native code.
     System.setProperty("org.jessies.libraryDirectories", "/home/mth/devel/terminator/terminator/.generated/i386_Linux/lib/");
-    System.setProperty("org.jessies.terminator.logDirectory", "/home/local/mth/tmp");
-    System.setProperty("org.jessies.terminator.optionsFile", "");
+    
+    final File dotDir = new File(HOME_DIR, ".terminator");
+    final  File logsDir = new File(dotDir, "logs");
+    dotDir.mkdir();
+    // FIXME: Permissions?
+    logsDir.mkdir();
+    
+    System.setProperty("org.jessies.terminator.logDirectory", logsDir.toString());
+    System.setProperty("org.jessies.terminator.optionsFile", new File(dotDir, "options").toString());
+    // OS specific? Install specific? Generated from the ruby in bin/terminator.
     System.setProperty("org.jessies.terminator.signalMap", "29:POLL30:PWR7:BUS11:SEGV9:KILL31:SYS15:TERM6:IOT1:HUP19:STOP5:TRAP2:INT27:PROF24:XCPU17:CLD13:PIPE8:FPE26:VTALRM29:IO28:WINCH21:TTIN20:TSTP6:ABRT22:TTOU3:QUIT10:USR117:CHLD18:CONT4:ILL12:USR225:XFSZ23:URG14:ALRM0:EXIT");
     
     // Install the terminfo.  See bin/terminator.
     String termInfoDirName = System.getenv("TERMINFO");
     if (termInfoDirName == null) {
-      termInfoDirName = System.getProperty("user.home") + File.separatorChar + ".terminfo";
+      termInfoDirName = HOME_DIR + File.separatorChar + ".terminfo";
     }
     File termInfoDir = new File(termInfoDirName);
     installTermInfoIn(new File(termInfoDir, "t"));
     installTermInfoIn(new File(termInfoDir, "74"));
+
+    Terminator.getPreferences().readFromDisk();
+    // Initialize the preferences.  Without this Ctrl-L/D etc don't work.
+    Terminator.getSharedInstance().optionsDidChange();
   }
 
   private void installTermInfoIn(File directory) throws IOException {
