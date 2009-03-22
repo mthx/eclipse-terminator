c78d1b47a83d56bc88974b172d688c0c832216ff
Matt Hillsdon <matt@hillsdon.net>
2009-03-22 17:00:58 +0000

    Set the default colors to black on white as that's more at home inside Eclipse.

--- src/net/hillsdon/eclipse/terminator/TerminatorPlugin.java
+++ src/net/hillsdon/eclipse/terminator/TerminatorPlugin.java
@@ -1,5 +1,6 @@
 package net.hillsdon.eclipse.terminator;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,6 +11,7 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 import terminator.Terminator;
+import terminator.TerminatorPreferences;
 import e.util.FileUtilities;
 
 /**
@@ -19,6 +21,10 @@ import e.util.FileUtilities;
  */
 public class TerminatorPlugin extends AbstractUIPlugin {
 
+  // Copied from TerminatorPrefererences.  Perhaps we should model 'ColorScheme' explicitly.
+  private static final Color LIGHT_BLUE = new Color(0xb3d4ff);
+  private static final Color NEAR_BLACK = new Color(0x181818);
+  
   private static final String HOME_DIR = System.getProperty("user.home");
 
   @Override
@@ -30,8 +36,9 @@ public class TerminatorPlugin extends AbstractUIPlugin {
 
     // Consider using another directory to avoid clash with the GNOME terminator?
     final File dotDir = new File(HOME_DIR, ".terminator");
-    final  File logsDir = new File(dotDir, "logs");
-    final  File libsDir = new File(dotDir, "libs");
+    final File logsDir = new File(dotDir, "logs");
+    final File libsDir = new File(dotDir, "libs");
+    final File optionsFile = new File(dotDir, "options");
     dotDir.mkdir();
     // FIXME: Permissions?
     logsDir.mkdir();
@@ -44,7 +51,7 @@ public class TerminatorPlugin extends AbstractUIPlugin {
     copyLibToLibsDir(libsDir, "libpty.so");
     System.setProperty("org.jessies.libraryDirectories", libsDir.toString());
     System.setProperty("org.jessies.terminator.logDirectory", logsDir.toString());
-    System.setProperty("org.jessies.terminator.optionsFile", new File(dotDir, "options").toString());
+    System.setProperty("org.jessies.terminator.optionsFile", optionsFile.toString());
     // OS specific? Install specific? Generated from the ruby in bin/terminator.
     System.setProperty("org.jessies.terminator.signalMap", "29:POLL30:PWR7:BUS11:SEGV9:KILL31:SYS15:TERM6:IOT1:HUP19:STOP5:TRAP2:INT27:PROF24:XCPU17:CLD13:PIPE8:FPE26:VTALRM29:IO28:WINCH21:TTIN20:TSTP6:ABRT22:TTOU3:QUIT10:USR117:CHLD18:CONT4:ILL12:USR225:XFSZ23:URG14:ALRM0:EXIT");
     
@@ -56,12 +63,21 @@ public class TerminatorPlugin extends AbstractUIPlugin {
     File termInfoDir = new File(termInfoDirName);
     installTermInfoIn(new File(termInfoDir, "t"));
     installTermInfoIn(new File(termInfoDir, "74"));
+    initializePreferences();
+  }
 
-    Terminator.getPreferences().readFromDisk();
+  private void initializePreferences() {
+    TerminatorPreferences preferences = Terminator.getPreferences();
+    // Change the default color scheme to one that fits in better within the workspace.
+    preferences.put(TerminatorPreferences.BACKGROUND_COLOR, Color.WHITE);
+    preferences.put(TerminatorPreferences.FOREGROUND_COLOR, NEAR_BLACK);
+    preferences.put(TerminatorPreferences.CURSOR_COLOR, Color.BLUE);
+    preferences.put(TerminatorPreferences.SELECTION_COLOR, LIGHT_BLUE);
+    preferences.readFromDisk();
     // Initialize the preferences.  Without this Ctrl-L/D etc don't work.
     Terminator.getSharedInstance().optionsDidChange();
   }
-
+  
   private void installTermInfoIn(File directory) throws IOException {
     if (directory.exists() || directory.mkdirs()) {
       InputStream in = null;
