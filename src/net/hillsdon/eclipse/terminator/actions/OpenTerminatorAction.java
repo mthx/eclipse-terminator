39a72ba24c0dc71129d474f6ed138cbabdbd3283
Matt Hillsdon <matt@hillsdon.net>
2009-04-03 07:31:41 +0000

    Remove incorrect license/copyright text brought over from shellintegration.

--- src/net/hillsdon/eclipse/terminator/actions/OpenTerminatorAction.java
+++ src/net/hillsdon/eclipse/terminator/actions/OpenTerminatorAction.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2008 Matthew Hillsdon.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Matthew Hillsdon <matt@hillsdon.net>
- *******************************************************************************/
 package net.hillsdon.eclipse.terminator.actions;
 
 import static org.eclipse.ui.plugin.AbstractUIPlugin.imageDescriptorFromPlugin;
