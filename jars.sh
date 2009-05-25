5525ee3638121709f7158de1df1a6c23f143944f
Matt Hillsdon <matt@hillsdon.net>
2009-05-25 20:46:17 +0000

    Use get_native.py derived native libraries. This adds linux amd64 and win32.

--- jars.sh
+++ jars.sh
@@ -14,7 +14,6 @@ do
   if [[ -d $dir/lib/jars ]]; then
     cp $dir/lib/jars/* "$OUTPUT/lib/"
   fi
-  cp $dir/.generated/i386_Linux/lib/*.so $OUTPUT/
 done
   
 cp ../terminator/.generated/terminfo/terminator src/net/hillsdon/eclipse/terminator/resources/terminfo
