052122743bf57e5742244bacce818bf627b7a597
Matt Hillsdon <matt@hillsdon.net>
2009-06-23 17:03:29 +0000

    Include svn version information in the terminator and salma-hayek jar manifests.

--- jars.sh
+++ jars.sh
@@ -8,12 +8,15 @@ OUTPUT=`pwd`
 for dir in "../salma-hayek" "../terminator";
 do 
   make -C "$dir"
+  MANIFEST="`pwd`/$dir/tmpManifest"
+  echo "Implementation-Version: `svnversion $dir`" > $MANIFEST
   pushd "$dir/.generated/classes"
-  jar cf "$OUTPUT/lib/`basename $dir`.jar" *
+  jar cfm "$OUTPUT/lib/`basename $dir`.jar" $MANIFEST *
   popd
   if [[ -d $dir/lib/jars ]]; then
     cp $dir/lib/jars/* "$OUTPUT/lib/"
   fi
+  rm $MANIFEST
 done
   
 cp ../terminator/.generated/terminfo/terminator src/net/hillsdon/eclipse/terminator/resources/terminfo
